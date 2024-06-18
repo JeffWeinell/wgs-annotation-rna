e29aee0d1983d0465cd94156ab3385373f729965
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 19:58:31 -0400

    Update RNA_04_trinity.sh

--- bin/RNA_04_trinity.sh
+++ bin/RNA_04_trinity.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # path to settings file 'settings.config'
-SETTINGS_FILE=$1
+SETTINGS_FILE=${1}
 # name of current sample to process
-NAME=$2
+NAME=${2}
 
 # load settings
 source $SETTINGS_FILE $NAME
@@ -12,4 +12,4 @@ source $SETTINGS_FILE $NAME
 conda activate $MYENV
 
 # Assemble with trinity
-Trinity --full_cleanup --seqType fq  --left ${R1PE_TRIMMED} --right ${R2PE_TRIMMED} --single ${R1S_TRIMMED} ${R2S_TRIMMED} --normalize_reads --CPU $NUM_CPU_TRINITY --max_memory $MAX_MEM_TRINITY --output $DIR_TRINITY_OUT
+Trinity --full_cleanup --seqType fq  --left $R1PE_TRIMMED --right $R2PE_TRIMMED --single $R1S_TRIMMED $R2S_TRIMMED --normalize_reads --CPU $NUM_CPU_TRINITY --max_memory $MAX_MEM_TRINITY --output $DIR_TRINITY_OUT
