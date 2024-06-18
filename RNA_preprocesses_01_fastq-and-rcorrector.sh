bd8bd25718d86f0448aa1f81b6bdd112f7b3758f
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 17:20:50 -0400

    Create RNA_preprocesses_01_fastq-and-rcorrector.sh

---
+++ RNA_preprocesses_01_fastq-and-rcorrector.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SETTINGS_FILE=${1}  # path to settings file
+NAME=${2}           # sample name (file name prefix for sample's raw reads)
+
+source $SETTINGS_FILE
+
+mkdir -p $DIR_RCORRECT
+mkdir -p $DIR_FILTER
+mkdir -p $DIR_TRIM_OUT
+
+# 1. fastqc
+fastqc -o $DIR_FASTQC $R1 $R2 -t $NUM_THREADS
+
+# 2. kmer read corrections
+cd $DIR_RCORRECT
+run_rcorrector.pl -1 $R1 -2 $R2 -od $DIR_RCORRECT -t $NUM_THREADS
