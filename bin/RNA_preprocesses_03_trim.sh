5191999210bd13ae4e7bcc9adb03932abb6f7d6e
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 18:29:20 -0400

    Create RNA_preprocesses_03_trim.sh

---
+++ bin/RNA_preprocesses_03_trim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SETTINGS_FILE=${1}
+NAME=${2}
+
+source $SETTINGS_FILE $NAME
+
+conda activate $MYENV
+
+trimmomatic PE -threads $NUM_THREADS *R1*.fq.gz *R2*.fq.gz ${R1PE_TRIMMED} ${R1S_TRIMMED} ${R2PE_TRIMMED} ${R2S_TRIMMED} ILLUMINACLIP:${ADAPTERS}:2:30:10:2:keepBothReads LEADING:3 TRAILING:3 MINLEN:36
