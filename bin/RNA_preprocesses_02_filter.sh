cf38252af9b328dadb039e40b198c5b5c0ef0bdc
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 18:14:47 -0400

    Create RNA_preprocesses_02_filter.sh

---
+++ bin/RNA_preprocesses_02_filter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# path to settings file 'settings.config'
+SETTINGS_FILE=${1}
+# name of the current sample
+NAME=${2}
+
+# load settings
+source $SETTINGS_FILE $NAME
+
+# see README.md for details on conda environment
+conda activate $MYENV
+
+# Filter/correct reads
+cd $DIR_FILTER
+
+python $PATH_FILTER_PY -1 $R1_COR -2 $R2_COR
+mv ${DIR_RCORRECT}/unfixrm_* $DIR_FILTER
