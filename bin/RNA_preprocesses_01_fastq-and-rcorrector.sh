b1bcb88c163cef66f0e2f3a1173f045e99f0468c
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 18:17:43 -0400

    Update RNA_preprocesses_01_fastq-and-rcorrector.sh

--- bin/RNA_preprocesses_01_fastq-and-rcorrector.sh
+++ bin/RNA_preprocesses_01_fastq-and-rcorrector.sh
@@ -3,11 +3,15 @@
 SETTINGS_FILE=${1}  # path to settings file
 NAME=${2}           # sample name (file name prefix for sample's raw reads)
 
-source $SETTINGS_FILE
+# load values for variables
+source $SETTINGS_FILE $NAME
 
 mkdir -p $DIR_RCORRECT
 mkdir -p $DIR_FILTER
 mkdir -p $DIR_TRIM_OUT
+
+# see README.md for creating this environment
+conda activate $MYENV
 
 # 1. fastqc
 fastqc -o $DIR_FASTQC $R1 $R2 -t $NUM_THREADS
