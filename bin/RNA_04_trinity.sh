91997aff13275b2e59598b53dd9d17a66c12b653
Jeffrey Weinell <jweine2@gmail.com>
2024-06-18 20:19:22 -0400

    Update RNA_04_trinity.sh

--- bin/RNA_04_trinity.sh
+++ bin/RNA_04_trinity.sh
@@ -12,4 +12,4 @@ source $SETTINGS_FILE $NAME
 conda activate $MYENV
 
 # Assemble with trinity
-Trinity --full_cleanup --seqType fq  --left $R1PE_TRIMMED --right $R2PE_TRIMMED --single $R1S_TRIMMED $R2S_TRIMMED --normalize_reads --CPU $NUM_CPU_TRINITY --max_memory $MAX_MEM_TRINITY --output $DIR_TRINITY_OUT
+Trinity --full_cleanup --seqType fq  --left $R1PE_TRIMMED --right $R2PE_TRIMMED --single $R1S_TRIMMED $R2S_TRIMMED --normalize_reads --CPU $NUM_CPU_TRINITY --max_memory $MAX_MEM_TRINITY --output $DIR_TRINITY
